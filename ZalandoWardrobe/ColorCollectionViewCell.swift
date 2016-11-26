87cbed47573824db6a7bfccd0d089ab8f66b9acd
filletofish <julik103@mail.ru>
2016-11-26 10:56:05 +0200

    Add collection View and Cell

---
+++ ZalandoWardrobe/ColorCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ColorCollectionViewCell.swift
+//  ZalandoWardrobe
+//
+//  Created by Филипп Федяков on 26.11.16.
+//  Copyright © 2016 AKarpov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorCollectionViewCell : UICollectionViewCell {
+    
+}
