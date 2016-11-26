19d79bad59ea4d057993deae844b5262381530d7
Alexander Karpov <x1te.rus@gmail.com>
2016-11-26 10:27:54 +0200

    added some UIImage APIs & initial network layer

---
+++ ZalandoWardrobe/APIDataDownloader.swift
@@ -0,0 +1,17 @@
+//
+//  APIDataDownloader.swift
+//  ZalandoWardrobe
+//
+//  Created by Alexander Karpov on 26.11.16.
+//  Copyright © 2016 AKarpov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/*
+ Add distinct static let API endpoint classes here
+ */
+class APIDataDownloader {
+    
+}
