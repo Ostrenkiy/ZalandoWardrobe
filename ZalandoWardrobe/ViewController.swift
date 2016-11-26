aa262c25a31ddcb3289dd93facc4b36f8b8b806c
filletofish <julik103@mail.ru>
2016-11-26 10:29:58 +0200

    Add image adding

--- ZalandoWardrobe/ViewController.swift
+++ ZalandoWardrobe/ViewController.swift
@@ -9,37 +9,26 @@
 import UIKit
 import ImagePicker
 
-class ViewController: UIViewController {
-    
-    var imagePicked: UIImage?
-
-    @IBOutlet weak var addItemButton: UIButton!
-    
+class ViewController: UIViewController {    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func addItem(_ sender: Any) {
         showImagePickerController()
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        switch segue.identifier! {
-//        case "AddItemSegue":
-//            guard let addItemVC = segue.destination as? AddItemViewController, let image = sender as? UIImage else {
-//                return
-//            }
-//            addItemVC.itemImage = image
-//        default:
-//            return
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier! {
+        case "AddItemSegue":
+            guard let addItemVC = segue.destination as? AddItemViewController, let image = sender as? UIImage else {
+                return
+            }
+            addItemVC.itemImage = image
+        default:
+            return
+        }
+    }
 }
 
 extension ViewController : ImagePickerDelegate {
@@ -51,19 +40,14 @@ extension ViewController : ImagePickerDelegate {
     }
     
     func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
-        print("3")
-        
     }
+    
     func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
-        print("3")
-
-       // self.imagePicked = images[0]
-        //self.performSegue(withIdentifier: "AddItemSegue", sender: images[0])
+        imagePicker.dismiss(animated: true, completion: nil)
+        self.performSegue(withIdentifier: "AddItemSegue", sender: images[0])
     }
     
     func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
-        print("1")
     }
-    
 }
 
